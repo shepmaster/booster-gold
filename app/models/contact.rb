cf64f5c9fc54caff7e043c449af8f14c9058c17b
Jake Goulding <jake.goulding@gmail.com>
2021-03-02 13:40:23 -0500

    Add validations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,14 @@
 class Contact < ApplicationRecord
+  validates :name, inclusion: { in: %w(jake leandro), message: "This is a no-%{value} club." }
+
+  validates :email, format: { with: /cow/, message: "only cows!" }
+
+  validate :dates_in_order, if: -> { start_at.present? && end_at.present? }
+
+  def dates_in_order
+    if start_at > end_at
+      errors.add(:start_at, 'needs to be before the end at')
+      errors.add(:end_at, 'needs to be after the start at')
+    end
+  end
 end
