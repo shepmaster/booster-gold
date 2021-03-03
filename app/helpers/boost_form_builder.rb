29660cf48129e8a79ca9d3e71b48fdc670194610
Jake Goulding <jake.goulding@gmail.com>
2021-03-03 16:21:59 -0500

    css up that form

--- app/helpers/boost_form_builder.rb
+++ app/helpers/boost_form_builder.rb
@@ -9,6 +9,32 @@ class BoostFormBuilder < ActionView::Helpers::FormBuilder
     end
   end
 
+  def jpg_text_field(method, options = {})
+    defs = { data: { reflex: 'change' } }
+    opts = defs.merge(options)
+
+    in_error = object.errors[method].presence
+    has_value = object.send(method).present?
+
+    cls, txt =
+         if in_error && has_value
+           ['text-red-700', "This has errors: #{in_error.join(', ')}"]
+         elsif in_error
+           ['text-blue-700', 'This is required']
+         else
+           ['text-green-700', 'This is OK']
+         end
+
+    txt = @template.tag.p(class: cls) do
+      txt.html_safe
+    end
+
+    @template.tag.div do
+      self.label(method, class: "font-extrabold #{cls}") +
+        text_field(method, opts) + txt
+    end
+  end
+
   def bst_text_area(method, options = {})
     defs = { data: { reflex: 'change' } }
     opts = defs.merge(options)
--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,10 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+# LOL where should this go?
+# Removes field_with_errors from form builder
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
