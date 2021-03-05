9f1654f09263dfb95417863913db0c1f1f7d9609
diachini <danny@jetpackworkflow.com>
2021-03-05 08:10:22 -0500

    Get a dropdown menu appearing for cards

--- app/javascript/controllers/index.ts
+++ app/javascript/controllers/index.ts
@@ -6,6 +6,7 @@ import { definitionsFromContext } from "stimulus/webpack-helpers";
 import StimulusReflex from 'stimulus_reflex'
 import consumer from '../channels/consumer'
 import controller from '../controllers/application_controller'
+import { Dropdown, Toggle } from "tailwindcss-stimulus-components"
 
 const application = Application.start();
 const context = require.context("controllers", true, /_controller\.[jt]s$/);
@@ -14,6 +15,8 @@ application.load(definitionsFromContext(context));
 // Load all the controllers within this directory and all subdirectories.
 // Controller files must be named *_controller.js.
 
+application.register('dropdown', Dropdown);
+application.register('toggle', Toggle);
 
 StimulusReflex.initialize(application, { consumer, controller, isolate: true })
 StimulusReflex.debug = process.env.RAILS_ENV === 'development'
