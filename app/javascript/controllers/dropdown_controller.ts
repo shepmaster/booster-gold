c66af86e561c3d264302a668fdf22b741e6ff107
Leandro Matioli <matioli.leandro@gmail.com>
2021-03-05 11:00:48 -0300

    Stop using tailwinds stimulus controllers.
    We had a stimulus controller defined for the dropdown component,
    but importing tailwinds stimulus controllers overwrites stuff and
    just makes things go in a different direction that expected with
    Stimulus controllers.
    
    When you open a file, you see the controller defined and you expect
    that all interactions will be happening inside that related controller
    js file.

--- app/javascript/controllers/dropdown_controller.ts
+++ app/javascript/controllers/dropdown_controller.ts
@@ -1,18 +1,13 @@
 // dropdown_controller.js
 import {Controller} from "stimulus"
-import {enter, leave} from 'el-transition';
 
 export default class extends Controller {
     static targets = ["menu", "button"]
+    static values = { open: Boolean }
 
-    declare menuTarget: HTMLInputElement;
-
-    toggleMenu() {
-      console.log("Yeah.. toggling menu")
-      if(this.menuTarget.classList.contains('hidden')) {
-          enter(this.menuTarget)
-      } else {
-          leave(this.menuTarget)
-      }
+    toggle() {
+      console.log('toogling');
+      this.openValue = !this.openValue;
+      this.menuTarget.classList.toggle("hidden");
     }
 }
