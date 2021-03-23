af6eef69f2714d515d169ebd324993f5e37b4590
Leandro Matioli <matioli.leandro@gmail.com>
2021-03-23 17:36:07 -0300

    Override default text_field instead of using a custom name.
    Also changes implementation to get classes/validation text.

--- app/helpers/boost_form_builder.rb
+++ app/helpers/boost_form_builder.rb
@@ -9,29 +9,21 @@ class BoostFormBuilder < ActionView::Helpers::FormBuilder
     end
   end
 
-  def jpg_text_field(method, options = {})
+  def text_field(method, options = {})
     defs = { data: { reflex: 'change' } }
     opts = defs.merge(options)
 
-    in_error = object.errors[method].presence
-    has_value = object.send(method).present?
+    @in_error = object.errors[method].presence
+    @has_value = object.send(method).present?
 
-    cls, txt =
-         if in_error && has_value
-           ['text-red-700', "This has errors: #{in_error.join(', ')}"]
-         elsif in_error
-           ['text-blue-700', 'This is required']
-         else
-           ['text-green-700', 'This is OK']
-         end
+    field = super(method, opts.merge({class: border_css}))
 
-    txt = @template.tag.p(class: cls) do
-      txt.html_safe
+    validation = @template.tag.p(class: text_css) do
+      validation_text.html_safe
     end
 
     @template.tag.div do
-      self.label(method, class: "font-extrabold #{cls}") +
-        text_field(method, opts) + txt
+      label(method, class: "font-extrabold") + field + validation
     end
   end
 
@@ -42,6 +34,38 @@ class BoostFormBuilder < ActionView::Helpers::FormBuilder
     @template.tag.div do
       self.label(method, class: 'font-extrabold') +
         text_area(method, opts)
+    end
+  end
+
+  private
+
+  def text_css
+    if @in_error && @has_value
+      'text-validation-error'
+    elsif @in_error
+      'text-validation-required'
+    else
+      'text-validation-ok'
+    end
+  end
+
+  def border_css
+    if @in_error && @has_value
+      'border-validation-error'
+    elsif @in_error
+      'border-validation-required'
+    else
+      'border-validation-ok'
+    end
+  end
+
+  def validation_text
+    if @in_error && @has_value
+      "Error: #{@in_error.join(', ')}"
+    elsif @in_error
+      'Required'
+    else
+      'OK'
     end
   end
 end
